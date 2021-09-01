b520bac260193bba4a3f5484f230b3442b5a4982
Yana Kurenkova <yakurenkova@gmail.com>
2021-09-01 10:02:04 +0200

    Update right_justify.py

--- basics/functions/right_justify.py
+++ basics/functions/right_justify.py
@@ -1,22 +1,19 @@
 # -*- coding: utf-8 -*-
 """
-Think Python: How to Think Like a Computer Scientist
-Allen B. Downey
-
-Chapter 3  Functions
-
-Exercise 1  
-Write a function named right_justify that takes a string named s as a parameter 
-and prints the string with enough leading spaces so that the last letter of the 
-string is in column 70 of the display.
-
->>> right_justify('monty')
-                                                                 monty
-Hint: Use string concatenation and repetition. Also, Python provides a built-in 
-function called len that returns the length of a string, so the value of 
-len('monty') is 5.
+This exercise is from the book "Think Python: How to Think Like a Computer 
+Scientist" by Allen B. Downey
 """
+
 def right_justify(string):
+    """
+    Takes a string as a parameter and prints the string with enough leading 
+    spaces so that the last letter of the string is in column 70 of the display.
+    
+    Example:
+    >>> right_justify('monty')
+                                                                 monty
+
+    """
     width = 70
     spaces_count = width-len(string)
     spaces = " " * spaces_count
